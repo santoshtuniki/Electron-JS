a6fe1be66acf22e4d515f976881f8850d8d50029
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-21 17:07:51 +0530

    child window using normal Anchor tag

---
+++ ChildWindow No-Renderer/Readme.md
@@ -0,0 +1,5 @@
+## The Child window will always show on top of the Parent window.
+
+## The Child window formed due to normal Anchor tag has all buttons Close(X), Maximize([]) and Minimize(-) button 
+
+## The Child window is independent of the controls of the Parent window
---
+++ ChildWindow No-Renderer/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        backgroundColor: '#F7C136'
+    });
+
+    win.loadFile('index.html');
+    // win.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+    createWindow()
+});
