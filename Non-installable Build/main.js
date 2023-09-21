0f3cd1ac604bd09a5ad9d3478e570f3b65fdce1e
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-21 20:08:48 +0530

    updates2

---
+++ Non-installable Build/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: __dirname + '/preload.js',
+            contextIsolation: true,
+        },
+        backgroundColor: '#F7C136'
+    });
+
+    ipcMain.on('msg', (event, data) => {
+        console.log(data)
+        event.reply('reply', 'Thanks for the data')
+    });
+
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
+
+};
+
+app.whenReady().then(() => {
+    createWindow()
+});
