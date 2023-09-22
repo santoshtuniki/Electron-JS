abeb8e2d1745eb639f40fd4b22f389b29e59140b
Saisantoshkumar_Tuniki <saisantosh.tuniki@onpasive.com>
2023-09-22 16:09:20 +0530

    Frameless but draggable app

---
+++ Frameless App/main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false,
+        backgroundColor: '#F7C136',
+        icon: 'browser-icon.png',
+    });
+
+    win.loadFile('index.html')
+}
+
+// app.whenReady().then(() => {
+//     createWindow()
+// });
+
+app.on('ready', () => {
+    console.warn(app.isReady())
+    createWindow()
+})
